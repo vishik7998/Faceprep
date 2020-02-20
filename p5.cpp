7b426b8dde41403ce45a160ce00ea92dfd235670
vishik <rana.vishik@gmail.com>
2020-02-20 18:13:40 +0530

    7th commit

---
+++ p5.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cstdio>
+#include<ctype.h>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int ch;
+    cin>>ch;
+    if(ch>0)
+    {
+        cout<<"Positive Number";
+    }
+    else if(ch<0){
+        cout<<"Negative number";
+    }
+    return 0;
+}
