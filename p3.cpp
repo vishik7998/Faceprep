7ad1f79f7480cbf5fa55796f2eb11dc19f6762fb
vishik <rana.vishik@gmail.com>
2020-02-20 17:13:22 +0530

    5th commit

---
+++ p3.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<conio.h>
+#include<ctype.h>
+#include<bits/stdc++.h>
+#include<cstdio>
+using namespace std;
+int main()
+{
+    char ch;
+    cin>>ch;
+    printf("%d", ch);
+    return 0;
+}
